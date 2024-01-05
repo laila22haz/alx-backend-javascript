3e0270a97bacb4798db51368248ffd9da687b1fe
laila22haz <hazmirlaila316@gmail.com>
2024-01-05 19:29:24 +0100

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,12 @@
-import { error } from "console"
-
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success === true) {
-            resolve({
-                status: 200,
-                body: 'Success'
-            })
-        } else {
-            reject(Error('The fake API is not working currently'));
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
