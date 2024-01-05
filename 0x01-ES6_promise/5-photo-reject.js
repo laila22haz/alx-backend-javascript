a78c517a150013c81cbb31b0036744cf16780181
laila22haz <hazmirlaila316@gmail.com>
2024-01-06 00:54:48 +0100

    Task 5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
-    return Promise.reject(new Error (
-        `${fileName} cannot be processed`
-    ));
+  return Promise.reject(new Error(
+    `${fileName} cannot be processed`,
+  ));
 }
