b2436811d6a9a55d9c28560f2b315d81269a6d25
laila22haz <hazmirlaila316@gmail.com>
2024-01-10 00:01:51 +0100

    Task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (startString !== '' && set instanceof Set && typeof startString === 'string') {
+    const arrayFromSet = Array.from(set);
+    const filteredSet = arrayFromSet.filter((value) => value.startsWith(startString));
+    const mapset = filteredSet.map((value) => value.substring(startString.length));
+    return mapset.join('-');
+  }
+  return '';
+}
