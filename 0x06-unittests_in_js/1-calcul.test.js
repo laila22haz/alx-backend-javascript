f98393c94f7e18cbf1d669018928d0b97520a02e
laila22haz <hazmirlaila316@gmail.com>
2024-02-29 03:02:38 +0100

    Task 1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,17 +4,13 @@ const calculateNumber = require('./1-calcul');
 
 describe('Test suit', () => {
     it('test calculateNumber with integers', () => {
-        assert.equal(calculateNumber('SUM', 1, 1), 2);
-        assert.equal(calculateNumber('SUBTRACT', 2, 2), 0);
-        assert.equal(calculateNumber('DIVIDE', 10, 1), 10);
+        assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
     });
     it('test calculateNumber with negative', () => {
-        assert.equal(calculateNumber('SUBTRACT', -1, -2.3), -1);
-        assert.equal(calculateNumber('SUBTRACT', -1.3, -3.7), 2);
+        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     });
     it('test claculateNumber', () => {
-        assert.equal(calculateNumber('DIVIDE', 1.2, 3.7), 0);
-        assert.equal(calculateNumber('DIVIDE', 1.5, 3.7), 0);
+        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
         assert.equal(calculateNumber('DIVIDE', 1.5, 0), 'Error');
     });
     it('test claculateNumber', () => {
