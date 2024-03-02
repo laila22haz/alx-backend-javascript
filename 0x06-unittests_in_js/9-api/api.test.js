8aa7d95a4c43eaff67b1a63b34959bb0732f7507
laila22haz <hazmirlaila316@gmail.com>
2024-03-02 09:35:30 +0100

    Task 9

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Express app', () => {
+    it('test index page', (done) => {
+        request.get('http://localhost:7865/', (err, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+    it('test the id if it is number', (done) => {
+        request.get('http://localhost:7865/cart/123', (err, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Payment methods for cart 123');
+            done();
+        });
+    });
+    it('test id if it is not number', (done) => {
+        request.get('http://localhost:7865/cart/hello', (err, response, body) => {
+            expect(response.statusCode).to.equal(400);
+            done();
+        });
+    });
+})
