ffffb1f0f4a6c390ca15a2161a6313be3fb7de8e
laila22haz <hazmirlaila316@gmail.com>
2024-03-02 09:39:16 +0100

    Task 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -5,12 +5,14 @@ const port = 7865;
 app.listen(port, () => {
     console.log('API available on localhost port 7865')
 });
+
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system')
 });
+
 app.get('/cart/:id([0-9]+)', (req, res) => {
     const id = req.params.id;
     res.send(`Payment methods for cart ${id}`);
-})
+});
 
 module.exports = app;
